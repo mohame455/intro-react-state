242c09f4a6254bf97feec48c5c5af3aba2d24e2b
mohame455 <61824199+mohame455@users.noreply.github.com>
2020-12-29 19:48:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: 0,
+      isShow: false,
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  increase = () => {
+    this.setState({
+      inputValue: this.state.inputValue + 1,
+    });
+  };
+
+  decrease = () => {
+    this.setState({
+      inputValue: this.state.inputValue - 1,
+    });
+  };
+
+  handleShow = () => {
+    this.setState({
+      isShow: !this.state.isShow,
+    });
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        isShow: true,
+      });
+    },10000);
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          onClick={this.handleShow}
+          className={this.state.isShow ? "red" : "green"}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {!this.state.isShow ? "display counter" : "hide counter"}
+        </button>
+        {this.state.isShow ? (
+          <div>
+            <button onClick={this.decrease}>-</button>
+            <input
+              type="number"
+              placeholder="0"
+              value={this.state.inputValue}
+            />
+            <button onClick={this.increase}>+</button>
+          </div>
+        ) : (
+          <p>click on the button to show the counter</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
